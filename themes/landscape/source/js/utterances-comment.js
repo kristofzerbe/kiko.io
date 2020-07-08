6a9250b3b5e9199b224730729c4d6ae37cee5f90
Kristof Zerbe <kristof.zerbe@goetzfried-ag.com>
2020-07-08 19:00:08 +0200

    Revert utterances fix

--- themes/landscape/source/js/utterances-comment.js
+++ themes/landscape/source/js/utterances-comment.js
@@ -1,34 +1,34 @@
-// function insertUtterancesCommentBlock() {
-//     var commentTheme = "github-light";
-//     if(localStorage.getItem("theme")) {
-//       if(localStorage.getItem("theme") === "dark"){
-//         commentTheme = "github-dark";
-//       }
-//     }
-//     const scriptId = "comment-theme-script";
-//     const existingScript = document.getElementById(scriptId);
-//     if (!existingScript) {
-//       const commentScript = document.createElement("script");
-//       commentScript.id = scriptId;
-//       commentScript.src = "https://utteranc.es/client.js";
-//       commentScript.setAttribute("repo", "kristofzerbe/kiko.io");
-//       commentScript.setAttribute("issue-term", "pathname");
-//       commentScript.setAttribute("theme", commentTheme);
-//       commentScript.setAttribute("crossorigin", "anonymous");
-//       const placeholder = document.getElementById("comment-placeholder");
-//       placeholder.innerHTML = "";
-//       placeholder.appendChild(commentScript);
-//     }
-// }
+function insertUtterancesCommentBlock() {
+    var commentTheme = "github-light";
+    if(localStorage.getItem("theme")) {
+      if(localStorage.getItem("theme") === "dark"){
+        commentTheme = "github-dark";
+      }
+    }
+    const scriptId = "comment-theme-script";
+    const existingScript = document.getElementById(scriptId);
+    if (!existingScript) {
+      const commentScript = document.createElement("script");
+      commentScript.id = scriptId;
+      commentScript.src = "https://utteranc.es/client.js";
+      commentScript.setAttribute("repo", "kristofzerbe/kiko.io");
+      commentScript.setAttribute("issue-term", "pathname");
+      commentScript.setAttribute("theme", commentTheme);
+      commentScript.setAttribute("crossorigin", "anonymous");
+      const placeholder = document.getElementById("comment-placeholder");
+      placeholder.innerHTML = "";
+      placeholder.appendChild(commentScript);
+    }
+}
 
-// //observe theme change, to adjust comment block theme
-// var target = document.documentElement,
-//     observer = new MutationObserver(function(mutations) {
-//         mutations.forEach(function(mutation) {
-//             if (mutation.attributionName === "data-theme" );
-//                 insertUtterancesCommentBlock();
-//             });        
-//     }),
-//     config = { attributes: true };
-// observer.observe(target, config);
+//observe theme change, to adjust comment block theme
+var target = document.documentElement,
+    observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            if (mutation.attributionName === "data-theme" );
+                insertUtterancesCommentBlock();
+            });        
+    }),
+    config = { attributes: true };
+observer.observe(target, config);
  
