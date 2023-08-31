64c5e35ac27494454f7c5d561e884c449e41d53d
Kristof Zerbe <kristof.zerbe@gmail.com>
2023-08-31 23:22:56 +0200

    New Post

--- themes/landscape/scripts/helper-excerpt-plain.js
+++ themes/landscape/scripts/helper-excerpt-plain.js
@@ -3,8 +3,8 @@ const { JSDOM } = require("jsdom");
 hexo.extend.helper.register('excerpt_plain', function(excerpt){
 
   const dom = new JSDOM('<!DOCTYPE html>');
-  var tmp = dom.window.document.createElement("div");
-  tmp.innerHTML = excerpt;
-  return tmp.textContent || tmp.innerText;
+  let e = dom.window.document.createElement("div");
+  e.innerHTML = excerpt;
+  return e.textContent || e.innerText;
 
 });
