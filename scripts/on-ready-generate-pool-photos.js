7dd1dca0e417e75c680c5ed6100ef9500299cdd0
Kristof Zerbe <kristof.zerbe@gmail.com>
2023-09-11 00:15:41 +0200

    New Pool Photos

--- scripts/on-ready-generate-pool-photos.js
+++ scripts/on-ready-generate-pool-photos.js
@@ -3,7 +3,7 @@ const PoolPhotoGenerator = require("../lib/pool-photo-generator.cjs").PoolPhotoG
 hexo.on("ready", function() {
     
   const inboundFolder = "../photos_new";
-  const originalFolder = "../photos_original_transfer";
+  const originalFolder = "../photos_original";
   const poolFolder = "../static/pool";
 
   const generator = new PoolPhotoGenerator(inboundFolder, originalFolder, poolFolder);
