505c3de8f1a5bdc7f0c8bd6f731b9699c80c84a1
Kristof Zerbe <kristof.zerbe@gmail.com>
2023-11-20 19:17:09 +0100

    New Before Post Render Filter

--- scripts/filter-before-post-render-ensure-photograph.js
+++ scripts/filter-before-post-render-ensure-photograph.js
@@ -13,12 +13,12 @@ hexo.extend.filter.register('before_post_render', function(data) {
     if (!data.photograph.file) {
       log.info("Processing missing photograph on post '" + data.title + "'");
 
-      // const selector = new photoSelector();
-      // let photo = selector.pick();
+      const selector = new photoSelector();
+      let photo = selector.pick();
   
-      // data.photograph.file = photo.file;
-      // data.photograph.name = photo.name;
-      // data.photograph.link = photo.link;  
+      data.photograph.file = photo.file;
+      data.photograph.name = photo.name;
+      data.photograph.link = photo.link;  
     }
   }
   return data;
