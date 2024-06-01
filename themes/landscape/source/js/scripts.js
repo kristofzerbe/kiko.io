91a6858215bccf2371ffe2d5c498240a7a826c45
Kristof Zerbe <kristof.zerbe@gmail.com>
2024-06-01 17:07:03 +0200

    Archive Refactoring into Panel

--- themes/landscape/source/js/scripts.js
+++ themes/landscape/source/js/scripts.js
@@ -121,7 +121,7 @@ function isVisibleInViewPort(e) {
   return ((eBottom <= viewBottom) && (eTop >= viewTop));
 }
 function initImageViewportVisibility() {
-  $(".article-photo, .archive-article-photo, .img-link").each(function() { //.card-img, 
+  $(".article-photo, .panel-photo, .img-link").each(function() { //.card-img, 
     if (isVisibleInViewPort($(this))) {
       $(this).addClass("in-view");
     } else {
