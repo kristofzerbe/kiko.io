37e3e34c212c61b2bd5a82b87885ebd03362bca5
Kristof Zerbe <kristof.zerbe@gmail.com>
2025-04-22 11:45:16 +0200

    Peertube Video Title

--- scripts/tags/tag-peertube.js
+++ scripts/tags/tag-peertube.js
@@ -2,19 +2,20 @@
   Peertube Video Tag
 
   Syntax:
-  {% peertube "instance" "id" %}
+  {% peertube "instance" "id" "title" %}
 
 */
 hexo.extend.tag.register('peertube', function (args) {
 
   const [
     instance,
-    id
+    id,
+    title
   ] = args;
 
   let element = `
     <div class="video-container">
-    <iframe src="https://${instance}/videos/embed/${id}"frameborder="0" allowfullscreen loading="lazy"></iframe>
+    <iframe src="https://${instance}/videos/embed/${id}" title="${title}" frameborder="0" allowfullscreen loading="lazy"></iframe>
     </div>
   `;
 
