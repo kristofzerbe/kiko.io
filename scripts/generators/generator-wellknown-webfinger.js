37b3982e0c07227a39a1675de334266f271138a4
Kristof Zerbe <kristof.zerbe@gmail.com>
2025-07-06 12:04:15 +0200

    Improved DiscoveriesConverter ...

--- scripts/generators/generator-wellknown-webfinger.js
+++ scripts/generators/generator-wellknown-webfinger.js
@@ -22,22 +22,22 @@ hexo.extend.generator.register("wellknown-webfinger", async function() {
 
   const _path = ".well-known/webfinger";
 
-  //TODO: deactivated due to DNS problems @ 2025-07-02
-  // axios.get(url).then(response => {
-  //   let json = response.data;
-  //   hexo.route.set(_path, json);
-  // });
-  let json = `
-  {
-    "subject":"acct:kiko@indieweb.social",
-    "aliases":["https://indieweb.social/@kiko","https://indieweb.social/users/kiko"],
-    "links":[
-      {"rel":"http://webfinger.net/rel/profile-page","type":"text/html","href":"https://indieweb.social/@kiko"},
-      {"rel":"self","type":"application/activity+json","href":"https://indieweb.social/users/kiko"},
-      {"rel":"http://ostatus.org/schema/1.0/subscribe","template":"https://indieweb.social/authorize_interaction?uri={uri}"},
-      {"rel":"http://webfinger.net/rel/avatar","type":"image/png","href":"https://cdn.masto.host/indiewebsocial/accounts/avatars/109/335/735/147/916/622/original/fe70df1a31593609.png"}
-    ]
-  }
-  `;
-  hexo.route.set(_path, json);
+  axios.get(url).then(response => {
+    let json = response.data;
+    hexo.route.set(_path, json);
+  });
+  //HACK: deactivated due to DNS problems @ 2025-07-02
+  // let json = `
+  // {
+  //   "subject":"acct:kiko@indieweb.social",
+  //   "aliases":["https://indieweb.social/@kiko","https://indieweb.social/users/kiko"],
+  //   "links":[
+  //     {"rel":"http://webfinger.net/rel/profile-page","type":"text/html","href":"https://indieweb.social/@kiko"},
+  //     {"rel":"self","type":"application/activity+json","href":"https://indieweb.social/users/kiko"},
+  //     {"rel":"http://ostatus.org/schema/1.0/subscribe","template":"https://indieweb.social/authorize_interaction?uri={uri}"},
+  //     {"rel":"http://webfinger.net/rel/avatar","type":"image/png","href":"https://cdn.masto.host/indiewebsocial/accounts/avatars/109/335/735/147/916/622/original/fe70df1a31593609.png"}
+  //   ]
+  // }
+  // `;
+  // hexo.route.set(_path, json);
 });
