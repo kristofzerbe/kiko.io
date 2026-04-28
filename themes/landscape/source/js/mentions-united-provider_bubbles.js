98ba537fd205ba8f8a8a12c95d47dbad42d21072
Kristof Zerbe <kristof.zerbe@gmail.com>
2026-04-28 22:04:24 +0200

    MU Plugin Bubbles

--- themes/landscape/source/js/mentions-united-provider_bubbles.js
+++ themes/landscape/source/js/mentions-united-provider_bubbles.js
@@ -1,5 +1,8 @@
 /**
- * Sample for a Mentions United Provider plugin class for retreiving interactions from __PROVIDER__
+ * Sample for a Mentions United Provider plugin class for retreiving interactions from Bubbles
+ * 
+ * On the blog aggregator Bubbles you can vote for a post or you can interact with it via a underlying
+ * GoToSocial instance. This plugin retrieves both types of interactions.
  * 
  * @author Kristof Zerbe
  * @version 1.0.0
@@ -15,14 +18,18 @@
  * 
  * Supported type-verbs:
  *  - like
- *  - TODO: ...
+ *  - repost
+ *  - reply
  * 
  * Remarks:
- *  - bubbles.town holds the votes (LIKES) for a post and social.bubbles.town all other interactions
+ *  - bubbles.town has votes (LIKES) for a post and social.bubbles.town all other interactions
+ *  - social.bubbles.town (GoToSocial instance) needs an READ-ONLY API token to access the interactions.
+ *    
+ *    TODO ...
  */
 class MentionsUnitedProvider_Bubbles extends MentionsUnited.Provider {
   key = "Bubbles"; // must be unique across all provider plugins for registration
-  
+
   options = {
     syndicationUrl: ""
   }
@@ -37,7 +44,7 @@ class MentionsUnitedProvider_Bubbles extends MentionsUnited.Provider {
 
     //get needed information from syndicationUrl
     let bubblesUrl = new URL(this.options.syndicationUrl);
-    this.sourceId = bubblesUrl.pathname.split("/").pop();
+    this.entryId = bubblesUrl.pathname.split("/").pop();
   }
 
   /**
@@ -46,63 +53,97 @@ class MentionsUnitedProvider_Bubbles extends MentionsUnited.Provider {
    * 
    * Remarks:
    *  - Step 1: Get interactions (votes) and "fediverse_url" fom bubbles.town
-   *  - Step 2: Get interactions from social.bubbles.town (GoToSocail)
+   *  - Step 2: Get interactions from GoToSocial instance via bubbles.town API bridge
    */
   async retrieve(args) {
     const msg = `${this.constructor.name}: Retreiving interactions for '${this.options.syndicationUrl}'`;
     args.fStart(msg);
     
-    // Step 1
+    // Step 1: Bubbles entry
     const apiResponseBubbles = await fetch(this.bubblesApiUrl());
     const apiDataBubbles = await apiResponseBubbles.json();
-    let votesCount = apiDataBubbles.votes ?? 0;
-    const entryDataBubbles = Array(votesCount).fill(0).map(e => ({ 
-      type: "like", 
+
+    this.votes = apiDataBubbles.votes ?? 0;
+    this.fediId = apiDataBubbles.fediverse_url.split("/").pop();
+
+    const entryDataBubbles = Array(this.votes).fill(0).map(e => ({  
       url: this.options.syndicationUrl,
-      id: this.sourceId,
+      id: this.entryId,
       author: "Anonymous",
       avatar: `https://api.dicebear.com/9.x/glass/svg?seed=${this.#getseed(5)}&&backgroundColor=3a8a7f&size=128&scale=75`
     }));
-    let interactionsBubbles = this.#processJsonData(entryDataBubbles);
+    let interactionsVotes = this.#processJsonData(entryDataBubbles, "vote");
 
-    // Step 2
-    //TODO
-    let interactionsSocial = [];
+    // Step 2: Bubbles GoToSocial interactions for entry
+
+    // 2.1 - Reposts (Boosts)
+    let interactionsReblogged = [];
+    try {
+      const apiResponseReblogged = await fetch(this.rebloggedApiUrl());
+      const apiDataReblogged = await apiResponseReblogged.json();
+      interactionsReblogged = this.#processJsonData(apiDataReblogged ?? [], "repost");        
+    } 
+    catch (e) { console.error(e); }
+    finally { args.fCount(); }
+
+    // 2.2 - Likes
+    let interactionsFavorited = [];
+    try {
+      const apiResponseFavorited = await fetch(this.favoritedApiUrl());
+      const apiDataFavorited = await apiResponseFavorited.json();
+      interactionsFavorited = this.#processJsonData(apiDataFavorited ?? [], "like");        
+    } 
+    catch (e) { console.error(e); }
+    finally { args.fCount(); }
+    
+    // 2.3 - Replies
+    let interactionsContext = [];
+    try {
+      const apiResponseContext = await fetch(this.contextApiUrl());
+      const apiDataContext = await apiResponseContext.json();
+      interactionsContext = this.#processJsonData(apiDataContext.descendants ?? [], "reply");        
+    } 
+    catch (e) { console.error(e); }
+    finally { args.fCount(); }
 
     args.fCount();
     
     args.fEnd(msg);
-    return [...interactionsBubbles, ...interactionsSocial];
-
+    return [...interactionsVotes, ...interactionsReblogged, ...interactionsFavorited, ... interactionsContext];
   }
 
   //////////////////////////////////////////////////////////////////////////////////////////
   
-  bubblesApiUrl() { return `https://bubbles.town/api/entry/${this.sourceId}`; };
+  bubblesApiUrl() { return `https://bubbles.town/api/entry/${this.entryId}`; }
+  contextApiUrl() { return `https://bubbles.town/api/fediverse/statuses/${this.fediId}/context` };
+  favoritedApiUrl() { return `https://bubbles.town/api/fediverse/statuses/${this.fediId}/favourited_by`; }
+  rebloggedApiUrl() { return `https://bubbles.town/api/fediverse/statuses/${this.fediId}/reblogged_by`; }
 
   /**
    * Processes retrieved JSON data into flat array of Interaction 
    * @param {Array.<Object>} entries 
+   * @param {String} type 
    * @returns {Array.<MentionsUnited.Interaction>}
    */
-  #processJsonData(entries) {
+  #processJsonData(entries, type) {
     return entries.map((item) => {
-      return this.#convertToInteraction(item);
+      return this.#convertToInteraction(item, type);
     });
   }
   
   /**
    * Converts specific data object into Interaction
    * @param {Object} entry 
+   * @param {String} type 
    * @returns {MentionsUnited.Interaction}
    */
-  #convertToInteraction(entry) {
+  #convertToInteraction(entry, type) {
     let r = new MentionsUnited.Interaction();
 
     r.syndication.url = this.options.syndicationUrl;
 
-    r.type = entry.type;
-    // r.received = entry...
+    r.type = (type === "vote") ? "like" : type;
+    r.received = (type === "vote") ? null : entry.created_at;
 
     r.source.provider = this.key;
     r.source.origin = "bubbles";
@@ -111,13 +152,25 @@ class MentionsUnitedProvider_Bubbles extends MentionsUnited.Provider {
     r.source.id = entry.id;
     r.source.title = "";
 
-    r.author.name = entry.author;
-    r.author.avatar = entry.avatar;
-    r.author.profile = entry.url;
+    switch (type) {
+      case "vote":
+        r.author.name = entry.author;
+        r.author.avatar = entry.avatar;
+        r.author.profile = entry.url;
+        break;
+      case "reply":
+        r.author.name = entry.account.display_name;
+        r.author.avatar = entry.account.avatar;
+        r.author.profile = entry.account.url;
+        r.content.html = entry.content;
+        break;
+      default:
+        r.author.name = entry.display_name;
+        r.author.avatar = entry.avatar;
+        r.author.profile = entry.url;
+        break;
+    }
 
-    // r.content.text = entry...;
-    // r.content.html = entry...;
-    
     return r;
   }
 
